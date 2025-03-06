1c4de0bcba96ad8d159bb6289ecf5653964ad6e2
Гор манучарян <manpacha231@gmail.com>
2025-03-06 17:51:37 +0300

    Добавил README, .gitignore и базовую структуру проекта

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"test/sorting"
+)
+
+func main() {
+	arr := []int{5, 3, 8, 4, 2}
+	fmt.Println("До сортировки:", arr)
+	sorting.BubbleSort(arr)
+	fmt.Println("После сортировки:", arr)
+}
