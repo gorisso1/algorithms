c903c94bc2c985cba2f1462c5ff481504c0af838
Гор манучарян <manpacha231@gmail.com>
2025-03-07 12:51:05 +0300

    1.0.1

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	arr := []int{5, 3, 8, 4, 2}
 	fmt.Println("До сортировки:", arr)
-	sorting.BubbleSort(arr)
+	sorting.InsertionSort(arr)
 	fmt.Println("После сортировки:", arr)
 }
