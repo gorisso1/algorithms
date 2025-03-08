23705e739834c549470f570342514e3336dce96f
Гор манучарян <manpacha231@gmail.com>
2025-03-09 00:50:02 +0300

    Добавление сортировки Merge Sort

--- main.go
+++ main.go
@@ -8,6 +8,14 @@ import (
 func main() {
 	arr := []int{5, 3, 8, 4, 2}
 	fmt.Println("До сортировки:", arr)
+
+	// InsertionSort меняет массив внутри
 	sorting.InsertionSort(arr)
-	fmt.Println("После сортировки:", arr)
+	fmt.Println("После InsertionSort:", arr)
+
+	// MergeSort возвращает новый массив
+	arr = []int{5, 3, 8, 4, 2} // Сбрасываем массив
+	arr = sorting.MergeSort(arr)
+
+	fmt.Println("После MergeSort:", arr)
 }
